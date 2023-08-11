1335e4388a4098c37b7c1ec529cbef4fdd436829
umang718 <umangvirat@gmail.com>
2023-08-11 11:30:58 +0530

    Completed Backend part 9: Vercel Deployment, Time: 11:23:00

--- services/common.js
+++ services/common.js
@@ -14,7 +14,9 @@ exports.cookieExtractor = function (req) {
     token = req.cookies["jwt"];
   }
   //TODO : this is temporary token for testing without cookie
-  token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDIyMTk0YTgzNDEyZTVlNDg1ZTEyNyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjkxNDk0NDUyfQ.6CghRrqIDxSd1VCvix5_alg_TpY7-1n2wzd13AI-Vyc";
+  // token =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDUyYTZkNmI4ZjU0YTkwZDExMGY0MyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjkxNzMyNzA2fQ.cXSdgkExWwm8H2J0vTmkzCJzyUgwZya8VzBRIHhnXow";
+//   token =
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDUyYTZkNmI4ZjU0YTkwZDExMGY0MyIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjkxNjkxNjUwfQ.4IHPtOav3OFiFTMAnlcfD9akLATO8dikwg8AYE4jwr0"; 
   return token;
 };
