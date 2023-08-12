9efa455ba18c423e11a6ff0a3089c5ce09b4b09a
umang718 <umangvirat@gmail.com>
2023-08-12 23:36:32 +0530

    Completed Backend Part 11, Node last video time: 01:51:00

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,6 +5,7 @@ const {
   checkAuth,
   resetPasswordRequest,
   resetPassword,
+  logout,
 } = require("../controller/Auth");
 const passport = require("passport");
 
@@ -14,6 +15,7 @@ router
   .post("/signup", createUser)
   .post("/login", passport.authenticate("local"), loginUser)
   .get("/check", passport.authenticate("jwt"), checkAuth)
+  .get("/logout", logout)
   .post("/reset-password-request", resetPasswordRequest)
   .post("/reset-password", resetPassword);
 exports.router = router;
