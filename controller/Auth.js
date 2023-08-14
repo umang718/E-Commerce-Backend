dc208a25ef6414e5c620f3ef6848c6222aa43366
umang718 <umangvirat@gmail.com>
2023-08-14 22:34:45 +0530

    Changed link in auth.js controller.

--- controller/Auth.js
+++ controller/Auth.js
@@ -79,7 +79,10 @@ exports.resetPasswordRequest = async (req, res) => {
 
     // Also set token in email
     const resetPageLink =
-      "http://localhost:3000/reset-password?token=" + token + "&email=" + email;
+      "https://mern-e-commerce-psi.vercel.app/reset-password?token=" +
+      token +
+      "&email=" +
+      email;
     const subject = "reset password for e-commerce";
     const html = `<p>Click <a href='${resetPageLink}'>here</a> to Reset Password</p>`;
 
